a40604bba9d67c5b2564e6b11699394765abaf04
Youmin Lee <youmin0107@gmail.com>
2025-08-09 22:54:58 +0900

    fix: ESLint option

--- travel-planner/eslint.config.mjs
+++ travel-planner/eslint.config.mjs
@@ -11,6 +11,26 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
 
+  {
+    // 검사하지 않을 경로들
+    ignores: [
+      "**/node_modules/**",
+      "**/.next/**",
+      "**/dist/**",
+      "**/src/generated/**", // Prisma 등 자동 생성 코드
+    ],
+  },
+
+  {
+    // 커스텀 규칙
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-this-alias": "off",
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+];
 export default eslintConfig;
